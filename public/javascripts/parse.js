847fd81a510b2a5a562516b1e8f921011f36f170
Mikal Wessel <mwessel27@gmail.com>
2016-11-06 17:05:32 -0500

    Only single xls and xlsx files can be uploaded

--- public/javascripts/parse.js
+++ public/javascripts/parse.js
@@ -1,5 +1,10 @@
 //script to parse excel
   function mytest1() {
     var input = document.getElementById("test").value;
+    // if(typeof require !== 'undefined') XLS = require('xlsjs');
+    //   var workbook = XLS.readFile(input);
+    //
+    // var sheet_name_list = workbook.SheetNames;
+    // var Sheet1A1 = workbook.Sheets[sheet_name_list[0]]['A1'].v;
     window.alert(input);
   }
